edc91a0a13f1831e3e92e63a8b94de4cca38f9be
josipovich <josipovicns@gmail.com>
2018-09-01 23:07:33 +0200

    Improve Loading style

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import appStore from './../stores/appStore'
+import logo from './../loading-animation.svg'
 
 
 export default () => {
     return (
         <div className="loading">
+            <img src={logo} className="loading-animation" alt="logo" />
             {appStore.loadingText}
         </div>
     )
