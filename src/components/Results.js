a4de4802772e3cbe5ea744233fd510d7fba0bc59
Vedran Josipovic <v.josipovic@uptime-engineering.com>
2018-09-01 17:15:24 +0200

    Refactor of Results, added Warning component

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,30 +1,28 @@
 import React from 'react'
 import appStore from './../stores/appStore'
-import Current from './Current'
-import Forecast from './Forecast'
+import CurrentWeather from './Current'
+import ForecastWeather from './Forecast'
+import Warning from './Warning'
 import CityMap from './CityMap'
 import './../styles/Results.css'
 
 
-const Warning = () => <span>No city with that name found!</span>
+const somethingIsWrong = () => {
+    const cods = [`${appStore.current.cod}`, `${appStore.forecast.cod}`]
+    const badCods = ['401', '404', '400']
+    return cods.some(cod => badCods.includes(cod))
+}          
 
-const Results = () => {
-    const cityName = appStore.current.name
-    const location = [appStore.current.coord.lat, appStore.current.coord.lon]    
-    return (
-        <div className="results">
-            <div className="results-top-row">
-                <CityMap cityName={cityName} location={location} />
-                <Current current={appStore.current} />
+export default () => {
+    return somethingIsWrong() 
+        ? <Warning message={appStore.current.message} />
+        : (
+            <div className="results">
+                <div className="results-top-row">
+                    <CityMap />
+                    <CurrentWeather current={appStore.current} />
+                </div>
+                <ForecastWeather forecast={appStore.forecast} />        
             </div>
-            <Forecast forecast={appStore.forecast} />
-        </div>	
-    )
-}
-
-export default props => {
-    const somethingWentWrong =
-        appStore.current.cod === '404' || appStore.forecast.cod === '404'
-
-    return somethingWentWrong ? <Warning /> : <Results />
+        )
 }
---
+++ src/components/Warning.js
@@ -0,0 +1,4 @@
+import React from 'react'
+
+
+export default props => <div className="warning-msg">{props.message}</div>
