792b1c64adf219091f9a19b0481ff2d7c2b1177f
josipovich <josipovicns@gmail.com>
2018-09-01 22:40:13 +0200

    Refactor: weatherCodeToWeatherClass from swith to if else

--- src/lib/weatherCodeToWeatherClass.js
+++ src/lib/weatherCodeToWeatherClass.js
@@ -1,31 +1,12 @@
 export default code => {
-    let color
-    code += ''    
-    switch (true) {
-        case code[0] === '2':
-            color = 'thunderstorm'
-            break;
-        case code[0] === '3':
-            color = 'drizzle'
-            break;
-        case code[0] === '5':
-            color = 'rain'
-            break;
-        case code[0] === '6':
-            color = 'snow'
-            break;
-        case code[0] === '7':
-            color = 'atmosphere'
-            break;
-        case code === '800':
-            color = 'clear'
-            break;
-        case code[0] === '8':
-            color = 'clouds'
-            break;
-        default:
-            color = 'snow'
-            break;
-    }
-    return color
+    code += '' 
+    // info taken from https://openweathermap.org/weather-conditions 
+    if (code[0] === '2') return 'thunderstorm'
+    else if (code[0] === '3') return 'drizzle'
+    else if (code[0] === '5') return 'rain'
+    else if (code[0] === '6') return 'snow'
+    else if (code[0] === '7') return 'atmosphere'
+    else if (code === '800') return 'clear'
+    else if (code[0] === '8') return 'clouds'
+    else return 'snow'
 }
