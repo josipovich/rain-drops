c38e16b6b4a3cecde34ac2dffad08429b5fa9ef1
josipovich <josipovicns@gmail.com>
2018-09-01 22:27:19 +0200

    Refactor: ForecastLegens styles and key

--- src/components/ForecastLegend.js
+++ src/components/ForecastLegend.js
@@ -5,7 +5,7 @@ import appStore from './../stores/appStore'
 export default () => {
     const types = ['legend', ...appStore.weatherTypeList]
     const LegendCell = ({type}) => <div className={`legend-cell ${type}`}>{type}</div>
-    const LegendCells = types.map(type => <LegendCell type={type}/>)
+    const LegendCells = types.map((type, i) => <LegendCell key={i} type={type}/>)
 
     return (
         <div className="forecast-legend">
