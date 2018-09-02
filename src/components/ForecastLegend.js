1a408fddb26078a5ab47f7ec60e18e66dd46cc4b
josipovich <josipovicns@gmail.com>
2018-09-02 14:34:41 +0200

    Add sundown type to ForecastLegend

--- src/components/ForecastLegend.js
+++ src/components/ForecastLegend.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 
 export default ({weatherTypeList}) => {
-    const types = ['legend', ...weatherTypeList]
+    const types = ['legend', 'sundown', ...weatherTypeList]
     const LegendCell = ({type}) => <div className={`legend-cell ${type}`}>{type}</div>
     const LegendCells = types.map((type, i) => <LegendCell key={i} type={type}/>)
 
