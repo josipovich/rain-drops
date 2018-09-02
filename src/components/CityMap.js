f5898d446d6c3f67b0d43bd8b013c20ea674e16c
josipovich <josipovicns@gmail.com>
2018-09-02 14:00:42 +0200

    Fix issue where CityMap is not updated with new query results

--- src/components/CityMap.js
+++ src/components/CityMap.js
@@ -1,31 +1,43 @@
 import React, {Component} from 'react'
-import appStore from './../stores/appStore'
+import {view} from 'react-easy-state'
 import L from 'leaflet'
 
 
-export default class extends Component {
+
+class CityMaps extends Component {
     componentDidMount() {
-        this.cityName = appStore.current.name
-        this.location = [appStore.current.coord.lat, appStore.current.coord.lon]    
-        this.locationAdjusted = [this.location[0] + 2, this.location[1]]
+        this.locationAdjusted = [this.props.location[0] + 2, this.props.location[1]]
         this.layers = [
             L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             })
         ]
         this.map = L.map('city-map', {
-            center: this.locationAdjusted,
-            zoom: 3,
-            layers: this.layers
+              center: this.locationAdjusted
+            , zoom: 3
+            , layers: this.layers
         })
-        this.marker = L.marker(this.location).addTo(this.map)
-        this.tooltip = this.marker
-            .bindTooltip(this.cityName, {interactive: true})
+        this.marker = L.marker(this.props.location).addTo(this.map)
+        this.tooltip = this._addTooltip(this.props.cityName)           
+    }
+
+    componentDidUpdate({location, cityName}) {  
+        this.locationAdjusted = [location[0] + 2, location[1]]  
+        this.map.setView(this.locationAdjusted)
+        this.marker.setLatLng(location)
+        this.tooltip = this._addTooltip(cityName) 
+    }
+
+    _addTooltip(cityName) {
+        return this.marker
+            .bindTooltip(cityName, {interactive: true})
             .on('hover', () => this.openTooltip()
-        )            
+        ) 
     }
 
     render() {
         return <div id="city-map"></div>
     }
 }
+
+export default view(CityMaps)
